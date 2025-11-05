cebdce9cb2edf9b7436cfd8e961f737345dca87e
Arjun <menonarjun98@gmail.com>
2025-11-05 19:05:59 +0530

    add Card prefab

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(SpriteRenderer))]
+[RequireComponent(typeof(BoxCollider2D))]
+public class Card : MonoBehaviour
+{
+    [Header("Sprites")]
+    public Sprite frontSprite;
+    public Sprite backSprite;
+
+    [Header("Flip settings")]
+    public float flipDuration = 0.28f;
+    public bool startsFaceUp = false;
+
+    [Header("Events")]
+    public UnityEvent<Card> onFlipRequested;
+    public UnityEvent<Card> onFlipCompleted;
+
+    // Runtime
+    private SpriteRenderer sr;
+    private bool isFront = false;
+    private bool isAnimating = false;
+
+    void Awake()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        if (sr == null)
+            sr = gameObject.AddComponent<SpriteRenderer>();
+
+        isFront = startsFaceUp;
+        sr.sprite = isFront ? frontSprite : backSprite;
+
+        var bc = GetComponent<BoxCollider2D>();
+        if (bc == null) bc = gameObject.AddComponent<BoxCollider2D>();
+        bc.isTrigger = false;
+    }
+
+    void OnMouseDown()
+    {
+        if (isAnimating) return;
+
+        RequestFlip();
+    }
+
+    public void RequestFlip()
+    {
+        onFlipRequested?.Invoke(this);
+
+        StartCoroutine(FlipRoutine(!isFront));
+    }
+
+    public IEnumerator FlipRoutine(bool showFront)
+    {
+        isAnimating = true;
+        float half = flipDuration / 2f;
+        Vector3 start = transform.localScale;
+        Vector3 mid = new Vector3(0f, start.y, start.z);
+        float t = 0f;
+
+        while (t < half)
+        {
+            t += Time.deltaTime;
+            transform.localScale = Vector3.Lerp(start, mid, t / half);
+            yield return null;
+        }
+
+        isFront = showFront;
+        sr.sprite = isFront ? frontSprite : backSprite;
+
+        t = 0f;
+        while (t < half)
+        {
+            t += Time.deltaTime;
+            transform.localScale = Vector3.Lerp(mid, start, t / half);
+            yield return null;
+        }
+
+        transform.localScale = start;
+        isAnimating = false;
+
+        onFlipCompleted?.Invoke(this);
+    }
+
+    public bool IsFaceUp() => isFront;
+}
